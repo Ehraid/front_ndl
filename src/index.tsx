0bd5a3ca1f15164eb6879d14c8c3a618f90afd64
wwwazz <wwwazz333@gmail.com>
2023-12-06 23:08:07 +0100

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AppPage from './pages/app/AppPage';
+import TemplatePage from './pages/template/TemplatePage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppPage />,
+  },
+
+
+  //if we are debuging
+  (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ?
+    {
+      path: "/template",
+      element: <TemplatePage />
+    } : {}
+]);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/template/TemplatePage.tsx
@@ -0,0 +1,10 @@
+import './TemplatePage.css';
+
+
+export default function TemplatePage() {
+  return (
+    <div >
+      <h1 className='Title'>Template</h1>
+    </div>
+  );
+}
