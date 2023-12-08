225c3f0bb6b4cb290eef16b3acc9d3d4ab14248e
wwwazz <wwwazz333@gmail.com>
2023-12-08 03:58:03 +0100

    fix chrom

--- src/pages/calendar/CalendarPage.jsx
+++ src/pages/calendar/CalendarPage.jsx
@@ -1,11 +1,9 @@
 import './Calendar.scss';
 import Background from '../background/Background';
 import Foreground from '../foreground/Foreground';
-import Helmet from "react-helmet";
 import { useParams } from 'react-router-dom';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const resources = [{
@@ -34,91 +32,93 @@ const resources = [{
 }];
 
 export default function CalendarPage() {
-  
+
 
 
   let { id } = useParams();
 
-if(id==null){
-  id = 0;
-}else{
-  id = parseInt(id)-1;
+  if (id == null) {
+    id = 0;
+  } else {
+    id = parseInt(id) - 1;
 
-}
-const titre = resources[id].titre;
-const numero = resources[id].numero;
-const lien = resources[id].lien;
-const text  = resources[id].text;
-
-
-
-  $(document).ready(function () {
-
-    TweenLite.set(".card-wrapper");
-    TweenLite.set(".card", { transformStyle: "preserve-3d", transformOrigin: "left 50%", transformPerspective: 1800 });
-    TweenLite.set(".back", { rotationY: 180 });
-    TweenLite.set([".back", ".front"], { backfaceVisibility: "hidden" });
-    TweenLite.set(".card__contents", { scale: 0, autoAlpha: 0 });
+  }
+  const titre = resources[id].titre;
+  const numero = resources[id].numero;
+  const lien = resources[id].lien;
+  const text = resources[id].text;
 
 
-    $('.card-wrapper').bind({
-      click: function () {
-        TweenLite.to($(this).find(".card"), 0.8, { rotationY: -120, ease: Back.easeOut });
-        TweenLite.to(".card__contents", 1.2, { scale: 1, autoAlpha: 1, delay: 0.5, ease: Elastic.easeOut });
-      }
-    });
 
-    $('.close').bind({
-      click: function () {
-        TweenLite.to(".card__contents", 0.4, { scale: 0, autoAlpha: 0, ease: Power1.easeOut });
-        TweenLite.to(".card", 0.6, { rotationY: 0, delay: 0.5, ease: Power1.easeOut });
-      }
-    });
+  useEffect(()=>{
+    $(document).ready(function () {
 
-    TweenLite.set(".text-box", { autoAlpha: 0, y: '20px' });
-
-    /*$('.arrow--right').bind({
-        click: function() {
-         TweenLite.to(".text-box", 0.8, {autoAlpha: 1, y: '0px', ease:Power1.easeOut});
-                TweenLite.to(".text-box", 0.8, {autoAlpha: 0, y: '40px', delay: 2, ease:Power1.easeIn});
+      TweenLite.set(".card-wrapper");
+      TweenLite.set(".card", { transformStyle: "preserve-3d", transformOrigin: "left 50%", transformPerspective: 1800 });
+      TweenLite.set(".back", { rotationY: 180 });
+      TweenLite.set([".back", ".front"], { backfaceVisibility: "hidden" });
+      TweenLite.set(".card__contents", { scale: 0, autoAlpha: 0 });
+  
+  
+      $('.card-wrapper').bind({
+        click: function () {
+          TweenLite.to($(this).find(".card"), 0.8, { rotationY: -120, ease: Back.easeOut });
+          TweenLite.to(".card__contents", 1.2, { scale: 1, autoAlpha: 1, delay: 0.5, ease: Elastic.easeOut });
         }
-    });*/
-
-    //Flying Robin animation
-    TweenLite.set("#robin", { x: 1100, y: 30, autoAlpha: 0 }, 2);
-
-    var robin = document.getElementById("robin")
-    var wing = document.getElementById("wing")
-    var tl = new TimelineMax({ repeatDelay: 1.5 });
-
-    TweenLite.set(wing, { transformOrigin: "left top" });
-
-    tl.to(robin, 3, {
-      bezier: {
-        type: "soft",
-        values: [{ x: 1100, y: 30 }, { x: 700, y: 100 }, { x: 500, y: 0 }, { x: 300, y: 100 }, { x: 100, y: 220 }],
-        autoRotate: false,
-        ease: Power1.easeInOut,
-      }
-    }, 2)
-      .to(robin, 0.6, { rotation: 30, ease: Power1.easeInOut }, "-=3")
-      .to(robin, 0.2, { autoAlpha: 1 }, "-=3")
-      .to(wing, 0.6, { rotation: 30 }, "-=3")
-      .to(robin, 1, { rotation: -30, ease: Power1.easeInOut }, "-=2.4")
-      .to(wing, 0.6, { rotation: -40 }, "-=2.4")
-      .to(robin, 1, { rotation: 0, ease: Power1.easeInOut }, "-=1")
-      .to(wing, 0.6, { rotation: -10 }, "-=1")
-      .to(robin, 3, {
+      });
+  
+      $('.close').bind({
+        click: function () {
+          TweenLite.to(".card__contents", 0.4, { scale: 0, autoAlpha: 0, ease: Power1.easeOut });
+          TweenLite.to(".card", 0.6, { rotationY: 0, delay: 0.5, ease: Power1.easeOut });
+        }
+      });
+  
+      TweenLite.set(".text-box", { autoAlpha: 0, y: '20px' });
+  
+      /*$('.arrow--right').bind({
+          click: function() {
+           TweenLite.to(".text-box", 0.8, {autoAlpha: 1, y: '0px', ease:Power1.easeOut});
+                  TweenLite.to(".text-box", 0.8, {autoAlpha: 0, y: '40px', delay: 2, ease:Power1.easeIn});
+          }
+      });*/
+  
+      //Flying Robin animation
+      TweenLite.set("#robin", { x: 1100, y: 30, autoAlpha: 0 }, 2);
+  
+      var robin = document.getElementById("robin")
+      var wing = document.getElementById("wing")
+      var tl = new TimelineMax({ repeatDelay: 1.5 });
+  
+      TweenLite.set(wing, { transformOrigin: "left top" });
+  
+      tl.to(robin, 3, {
         bezier: {
           type: "soft",
-          values: [{ x: 100, y: 220 }, { x: -100, y: 100 }],
+          values: [{ x: 1100, y: 30 }, { x: 700, y: 100 }, { x: 500, y: 0 }, { x: 300, y: 100 }, { x: 100, y: 220 }],
           autoRotate: false,
-          ease: Power1.easeIn,
+          ease: Power1.easeInOut,
         }
-      }, "+=2")
-      .to(wing, 0.8, { rotation: -30, ease: Power1.easeInOut }, "-=3")
-      .to(wing, 0.8, { rotation: 10, ease: Power1.easeInOut }, "-=2.2")
-  });
+      }, 2)
+        .to(robin, 0.6, { rotation: 30, ease: Power1.easeInOut }, "-=3")
+        .to(robin, 0.2, { autoAlpha: 1 }, "-=3")
+        .to(wing, 0.6, { rotation: 30 }, "-=3")
+        .to(robin, 1, { rotation: -30, ease: Power1.easeInOut }, "-=2.4")
+        .to(wing, 0.6, { rotation: -40 }, "-=2.4")
+        .to(robin, 1, { rotation: 0, ease: Power1.easeInOut }, "-=1")
+        .to(wing, 0.6, { rotation: -10 }, "-=1")
+        .to(robin, 3, {
+          bezier: {
+            type: "soft",
+            values: [{ x: 100, y: 220 }, { x: -100, y: 100 }],
+            autoRotate: false,
+            ease: Power1.easeIn,
+          }
+        }, "+=2")
+        .to(wing, 0.8, { rotation: -30, ease: Power1.easeInOut }, "-=3")
+        .to(wing, 0.8, { rotation: 10, ease: Power1.easeInOut }, "-=2.2")
+    });
+  })
 
 
   return (
@@ -137,29 +137,13 @@ const text  = resources[id].text;
         src="https://cdn.discordapp.com/attachments/1175085539704176812/1182506910931484722/Jingle_Bells_Instrumental.mp3"
 
       />
-      <Helmet>
-        <script
-          src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"
-          async={false} defer={false}
-        ></script>
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/easing/EasePack.min.js"
-          async={false} defer={false}
-        ></script>
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"
-          async={false} defer={false}
-        ></script>
-      </Helmet>
-      {/* Hello world */}
+
       <div className="awesome" style={{ border: '1px solid red' }}>
         <div>
           <Foreground text={text} lien={lien} numero={numero} titre={titre} />
           <Background />
-        </div>  <label htmlFor="name">Enter your name: </label>
-        <input type="text" id="name" />
+        </div>
       </div>
-      <p>Enter your HTML here</p>
     </div>
   );
 };
